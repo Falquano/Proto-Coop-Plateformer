a855620f9629b0d362a7f48c5d06ab95780a348c
SebPautot <spautot.mgz@hotmail.com>
2022-02-01 19:25:08 +0100

    Player now can't use Push/Pull when stunned

--- Assets/Player/Player.cs
+++ Assets/Player/Player.cs
@@ -146,7 +146,7 @@ public class Player : MonoBehaviour
 
 	public void InputHelp(InputAction.CallbackContext context)
 	{
-		if (context.performed && help.CanHelp)
+		if (context.performed && help.CanHelp && State != PlayerState.Stun)
 		{
 			State = PlayerState.OfferingHelp;
 			sound.HelpSound();
@@ -158,7 +158,7 @@ public class Player : MonoBehaviour
 
 	public void InputPush(InputAction.CallbackContext context)
 	{
-		if (context.performed && help.CanHelp)
+		if (context.performed && help.CanHelp && State != PlayerState.Stun)
 		{
 			help.HelpMod = 1f;
 			State = PlayerState.OfferingHelp;
