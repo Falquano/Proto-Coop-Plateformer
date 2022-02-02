17b0a9619b79036ff340583ac5c959319ba4c16d
SebPautot <spautot.mgz@hotmail.com>
2022-02-02 19:38:32 +0100

    Added UpdateKing which returns true if there is a crowned player

--- Assets/MusicManagerManager.cs
+++ Assets/MusicManagerManager.cs
@@ -5,10 +5,13 @@ using UnityEngine;
 public class MusicManagerManager : MonoBehaviour
 {
 	private FMOD.Studio.EventInstance music;
-	
+	bool hasAKing = false;
+	PlayerManager playerManager;
 
 	private void Start()
 	{
+		if (playerManager == null)
+			playerManager = GameObject.Find("@Player Manager").GetComponent<PlayerManager>();
 		music = FMODUnity.RuntimeManager.CreateInstance("event:/updated SD/Music");
 		music.start();
 	}
@@ -17,6 +20,18 @@ public class MusicManagerManager : MonoBehaviour
     {
 		//music.setParameterByName("kingPresence", laVariableIsCrowned);
 
+	}
+
+	bool UpdateKing()
+	{
+		var x = false;
+		foreach(Player p in playerManager.Players)
+		{
+			if(p.characterController.IsCrowned)
+				return true;
+		}
+
+		return false;
 	}
 
 }
